dcfeafa9ee2f75c1af7fa8fcbd5da8f23360001c
hemanth.hm <hemanth.hm@gmail.com>
2014-08-13 21:20:06 +0530

    Using BookService Factory.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,35 @@
  * Controller of the liveMongoApp
  */
 angular.module('liveMongoApp')
-  .controller('MainCtrl', function($scope, $http) {
-
-    $http.get('http://localhost:1337/books').
-    success(function(resp) {
-      $scope.books = angular.fromJson(resp);
-    }).
-    error(function(err) {
-      console.error(err);
+  .controller('MainCtrl',['$scope','BookService',function($scope,BookService) {
+    $scope.books = BookService.getBooks.success(function(data){
+      $scope.books = data;
     });
+
     $scope.gridOptions = {
       data: 'books',
       enableCellSelection: true,
       enableRowSelection: false,
       enableCellEditOnFocus: true,
+    };
+
+    $scope.$on('ngGridEventEndCellEdit', function(evt){
+      console.log(evt.targetScope.row.entity);
+      //TDB save to DB.
+    });
+
+  }]).factory('BookService',function($http){
+    var getBooks =
+        $http.get('http://localhost:1337/books').
+      success(function(resp) {
+        return angular.fromJson(resp);
+      }).
+      error(function(err) {
+        console.error(err);
+      });
+
+    return {
+      getBooks : getBooks
     };
   });
 
